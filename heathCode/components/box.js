9bee7ad8ec615eacf4724794e25fc1628f7bae8c
linbao <5371973+linbaooooo@user.noreply.gitee.com>
2024-08-27 11:04:06 +0800

    组件修改

---
+++ heathCode/components/box.js
@@ -0,0 +1,30 @@
+// components/box.js
+Component({
+
+  /**
+   * 接收父组件传值
+   */
+  properties: {
+    created_at:{
+      type:String
+    },resulttext:{
+      type:String
+    },testing_organization:{
+      type:String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
